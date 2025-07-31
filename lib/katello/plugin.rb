cafba0787e6b7da607e980e9a283cff2cb64b4b2
Partha Aji <paji@redhat.com>
2025-07-31 14:19:44 -0400

    more

--- lib/katello/plugin.rb
+++ lib/katello/plugin.rb
@@ -64,8 +64,7 @@ Foreman::Plugin.register :katello do
     menu :top_menu,
          :sync_status,
          :caption => N_('Sync Status'),
-         :url_hash => {:controller => 'katello/sync_management',
-                       :action => 'index'},
+         :url => '/sync_management',
          :engine => Katello::Engine,
          :turbolinks => false
 
